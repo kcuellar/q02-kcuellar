b0a325c33fff86657a49c46fea91c5380e35ad15
Dr Kevin Buffardi <kbuffardi@csuchico.edu>
2017-10-09 16:37:08 +0000

    Functions and tests implemented

--- Piezas.cpp
+++ Piezas.cpp
@@ -13,23 +13,31 @@
  * [0,0][0,1][0,2][0,3]
  * So that a piece dropped in column 2 should take [0,2] and the next one
  * dropped in column 2 should take [1,2].
-**/
+ **/
 
 
 /**
  * Constructor sets an empty board (default 3 rows, 4 columns) and 
  * specifies it is X's turn first
-**/
+ **/
 Piezas::Piezas()
 {
+	turn = X;
+
+	board.resize(BOARD_ROWS);
+	for(int i=0; i<BOARD_ROWS; i++)
+		board[i].resize(BOARD_COLS, Blank);
 }
 
 /**
  * Resets each board location to the Blank Piece value, with a board of the
  * same size as previously specified
-**/
+ **/
 void Piezas::reset()
 {
+	for(int i=0; i<BOARD_ROWS; i++)
+		for(int j=0; j<BOARD_COLS; j++)
+			board[i][j] = Blank;
 }
 
 /**
@@ -39,19 +47,55 @@ void Piezas::reset()
  * In that case, placePiece returns Piece Blank value 
  * Out of bounds coordinates return the Piece Invalid value
  * Trying to drop a piece where it cannot be placed loses the player's turn
-**/ 
+ **/ 
 Piece Piezas::dropPiece(int column)
 {
-    return Blank;
+	if(column > 3 || column < 0) {
+		if(turn == X)
+			turn = O;
+		else
+			turn = X;
+		return Invalid;
+	}
+
+	// Check column to see if it is full.
+	// If not, place piece.
+	for(int i = 0; i < BOARD_ROWS; i++) { 
+		if(board[i][column] == Blank) {
+			board[i][column] = turn;
+
+			if(turn == X)
+				turn = O;
+			else
+				turn = X;
+
+			return board[i][column];
+		}
+	}
+
+	if(turn == X)
+		turn = O;
+	else
+		turn = X;
+
+	return Blank;
 }
 
 /**
  * Returns what piece is at the provided coordinates, or Blank if there
  * are no pieces there, or Invalid if the coordinates are out of bounds
-**/
+ **/
 Piece Piezas::pieceAt(int row, int column)
 {
-    return Blank;
+	if(row > 2 || column > 3 || row < 0 || column < 0) {
+		return Invalid;
+	}
+	else if (board[row][column] == Blank) {
+		return Blank;
+	}
+	else { // already a piece there
+		return board[row][column];
+	}
 }
 
 /**
@@ -62,8 +106,67 @@ Piece Piezas::pieceAt(int row, int column)
  * the most adjacent pieces in a single line. Lines can go either vertically
  * or horizontally. If both X's and O's have the same max number of pieces in a
  * line, it is a tie.
-**/
+ **/
 Piece Piezas::gameState()
 {
-    return Blank;
+	int x = 0;
+	int o = 0;
+	int adj = 0;
+
+	// Check if board is filled
+	for(int i=0; i<BOARD_ROWS; i++) {
+		for(int j=0; j<BOARD_COLS; j++) {
+			if(board[i][j] == Blank) {
+				return Invalid;
+			}
+		}
+	}
+
+	// Check horizontals
+	for(int i = 0; i < BOARD_ROWS; i++) {
+		for(int j = 0; j < BOARD_COLS; j++) {
+			// sum player with most adjacent pices in row
+			if(board[i][j] == board[i][j+1]) { 
+				adj++;
+				if((board[i][j] == X) && (adj > x))
+					x = adj;
+				else if((board[i][j] == O) && (adj > o))
+					o = adj;
+			}
+			else { // different piece, restart count
+				adj = 0;
+			}
+		}
+	}
+
+	adj = 0;
+
+	// Check verticals 
+	for(int j = 0; j < BOARD_COLS; j++) {
+		for(int i = 0; i < BOARD_ROWS; i++) {
+			// sum player with most adjacent pices in row
+			if(board[i][j] == board[i+1][j]) { 
+				adj++;
+				if((board[i][j] == X) && (adj > x))
+					x = adj;
+				else if((board[i][j] == O) && (adj > o))
+					o = adj;
+			}
+			else { // different piece, restart count
+				adj = 0;
+			}
+		}
+	}
+
+	// Player that has the most adjacent pieces in a single line wins
+	// Return Blank if tie
+	if(x > o) {
+		return X;
+	}
+	else if(o > x) {
+		return O;
+	} 
+	else {
+		return Blank;
+	}
 }
--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -1,10 +1,13 @@
 /**
  * Unit Tests for Piezas
-**/
+ **/
+
+#include <iostream>
+using namespace std;
 
 #include <gtest/gtest.h>
 #include "Piezas.h"
- 
+
 class PiezasTest : public ::testing::Test
 {
 	protected:
@@ -14,7 +17,163 @@ class PiezasTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor) 
 };
 
-TEST(PiezasTest, sanityCheck)
+/*TEST(PiezasTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
+}*/
+
+TEST(PiezasTest, resetBoard)
+{
+	Piezas game;
+	Piece one;
+	Piece two;
+	game.dropPiece(3);
+	game.dropPiece(3);
+	one = game.dropPiece(3);
+	game.reset();
+	two = game.pieceAt(2,3);
+	ASSERT_NE(one, two);
 }
+
+TEST(PiezasTest, placeX)
+{
+	Piezas game;
+	Piece one;
+	Piece two;
+	one = game.dropPiece(1);
+	two = game.pieceAt(0,1);
+	ASSERT_EQ(one, two);
+}
+
+TEST(PiezasTest, placeO)
+{
+	Piezas game;
+	Piece one;
+	Piece two;
+	game.dropPiece(1);
+	one = game.dropPiece(1);
+	two = game.pieceAt(1,1);
+	ASSERT_EQ(one, two);
+}
+
+
+TEST(PiezasTest, outOfBounds1)
+{
+	Piezas game;
+	Piece one;
+	one = game.dropPiece(-1);
+	ASSERT_EQ(one, Invalid);
+}
+
+TEST(PiezasTest, outOfBounds2)
+{
+	Piezas game;
+	Piece one;
+	one = game.dropPiece(4);
+	ASSERT_EQ(one, Invalid);
+}
+
+TEST(PiezasTest, columnFull)
+{
+	Piezas game;
+	Piece one;
+	game.dropPiece(3);
+	game.dropPiece(3);
+	game.dropPiece(3);
+	one = game.dropPiece(3);
+	ASSERT_EQ(one, Blank);
+}
+
+TEST(PiezasTest, noPiece)
+{
+	Piezas game;
+	Piece one;
+	game.dropPiece(2);
+	one = game.pieceAt(2,3);
+	ASSERT_EQ(one, Blank);
+}
+
+TEST(PiezasTest, pieceAtOut1)
+{
+	Piezas game;
+	Piece one;
+	one = game.pieceAt(-1,0);
+	ASSERT_EQ(one, Invalid);
+}
+
+TEST(PiezasTest, pieceAtOut2)
+{
+	Piezas game;
+	Piece one;
+	one = game.pieceAt(3,0);
+	ASSERT_EQ(one, Invalid);
+}
+
+TEST(PiezasTest, pieceAtOut3)
+{
+	Piezas game;
+	Piece one;
+	one = game.pieceAt(0,-1);
+	ASSERT_EQ(one, Invalid);
+}
+
+TEST(PiezasTest, pieceAtOut4)
+{
+	Piezas game;
+	Piece one;
+	one = game.pieceAt(0,4);
+	ASSERT_EQ(one, Invalid);
+}
+
+TEST(PiezasTest, notOver)
+{
+	Piezas game;
+	Piece one;
+	one = game.gameState();
+	ASSERT_EQ(one, Invalid);
+}
+
+TEST(PiezasTest, winner1X)
+{
+	Piezas game;
+	Piece one;
+	game.dropPiece(1);
+	game.dropPiece(2);
+	game.dropPiece(1);
+	game.dropPiece(3);
+	game.dropPiece(1);
+	game.dropPiece(0);
+
+
+	game.dropPiece(0);
+	game.dropPiece(0);
+	game.dropPiece(2);
+	game.dropPiece(2);
+	game.dropPiece(4);
+	game.dropPiece(3);
+	game.dropPiece(3);
+
+	one = game.gameState();
+	ASSERT_EQ(one, X);
+}
+
+TEST(PiezasTest, tie)
+{
+	Piezas game;
+	Piece one;
+	game.dropPiece(0);
+	game.dropPiece(1);
+	game.dropPiece(2);
+	game.dropPiece(3);
+	game.dropPiece(0);
+	game.dropPiece(1);
+	game.dropPiece(2);
+	game.dropPiece(3);
+	game.dropPiece(0);
+	game.dropPiece(1);
+	game.dropPiece(2);
+	game.dropPiece(3);
+	one = game.gameState();
+	ASSERT_EQ(one, Blank);
+}
+
